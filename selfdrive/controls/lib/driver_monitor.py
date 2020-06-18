80f6f49f06e81f150dfe20adcf3affe2a6083301
whrho <whrho@fasoo.com>
2020-06-18 11:23:19 +0900

    fix monitoring

--- selfdrive/controls/lib/driver_monitor.py
+++ selfdrive/controls/lib/driver_monitor.py
@@ -8,12 +8,12 @@ from selfdrive.kegman_conf import kegman_conf
 kegman = kegman_conf()
 
 
-_AWARENESS_TIME = min(int(kegman.conf['wheelTouchSeconds']), 600)    # x minutes limit without user touching steering wheels make the car enter a terminal status
+_AWARENESS_TIME = min(int(kegman.conf['wheelTouchSeconds'])+10, 600)    # x minutes limit without user touching steering wheels make the car enter a terminal status
 _AWARENESS_PRE_TIME_TILL_TERMINAL = 25.  # a first alert is issued 25s before expiration
 _AWARENESS_PROMPT_TIME_TILL_TERMINAL = 15.  # a second alert is issued 15s before start decelerating the car
-_DISTRACTED_TIME = 11.
-_DISTRACTED_PRE_TIME_TILL_TERMINAL = 8.
-_DISTRACTED_PROMPT_TIME_TILL_TERMINAL = 6.
+_DISTRACTED_TIME = 45.
+_DISTRACTED_PRE_TIME_TILL_TERMINAL = 15.
+_DISTRACTED_PROMPT_TIME_TILL_TERMINAL = 9.
 
 _FACE_THRESHOLD = 0.4
 _EYE_THRESHOLD = 0.6
